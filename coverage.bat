71229135710f07ff36f4bb3ed90f97536ac1caa0
markridgewell <65724979+markridgewell@users.noreply.github.com>
2022-06-17 21:04:01 +0200

    Fix coverage script adding third party code

--- coverage.bat
+++ coverage.bat
@@ -2,5 +2,5 @@
 setlocal
 SET PRESET=%1
 set GTEST_FILTER=%2
-OpenCppCoverage --export_type html:coverage --modules "*.exe" --sources source --cover_children --quiet -- ctest -V --preset %PRESET%
+OpenCppCoverage --export_type html:coverage --modules "*.exe" --sources "source\\" --cover_children --quiet -- ctest -V --preset %PRESET%
 endlocal
