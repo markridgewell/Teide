8b0f445011c9562026c106b701b786cf84dfdd4d
markridgewell <65724979+markridgewell@users.noreply.github.com>
2022-06-18 22:47:51 +0200

    Use latest version of clang-format-action in CI
    Attempt to fix switfshader for Windows CI

--- coverage.bat
+++ coverage.bat
@@ -2,5 +2,5 @@
 setlocal
 SET PRESET=%1
 set GTEST_FILTER=%2
-OpenCppCoverage --export_type html:coverage --modules "*.exe" --sources "source\\" --cover_children --quiet -- ctest -V --preset %PRESET%
+OpenCppCoverage --export_type html:coverage --modules "*.exe" --sources "Source\\" --excluded_sources "Test\\" --excluded_sources "build" --cover_children --quiet -- ctest -V --preset %PRESET%
 endlocal
