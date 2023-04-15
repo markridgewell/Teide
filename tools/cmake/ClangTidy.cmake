d527a3958a60441a0b198c680d369320525a6fb6
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-04-16 00:57:05 +0200

    Only run clang-tidy on modified files

--- tools/cmake/ClangTidy.cmake
+++ tools/cmake/ClangTidy.cmake
@@ -1,15 +1,9 @@
 include(tools/cmake/CompileCommands.cmake)
 
 function(td_add_clang_tidy)
-    find_program(clang_tidy "clang-tidy" REQUIRED)
-
     compile_commands()
 
-    set(clang_tidy_cmd "${clang_tidy}")
-    foreach(source ${project_sources})
-        list(APPEND clang_tidy_cmd ${source})
-    endforeach()
-    list(APPEND clang_tidy_cmd "--" "-std=c++${CMAKE_CXX_STANDARD}")
+    list(APPEND clang_tidy_cmd "-std=c++${CMAKE_CXX_STANDARD}")
     foreach(dir IN LISTS project_include_directories)
         list(APPEND clang_tidy_cmd "-I${dir}")
     endforeach()
@@ -22,8 +16,8 @@ function(td_add_clang_tidy)
 
     add_custom_target(
         ClangTidy
-        COMMAND ${clang_tidy} --version
-        COMMAND ${clang_tidy_cmd}
+        COMMAND ${CMAKE_COMMAND} "-DSOURCES=${project_sources}" "-DCLANG_TIDY_ARGS=${clang_tidy_cmd}" -P
+                ${CMAKE_SOURCE_DIR}/tools/cmake/scripts/RunClangTidy.cmake
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
     set_target_properties(ClangTidy PROPERTIES FOLDER StaticAnalysis)
---
+++ tools/cmake/scripts/RunClangTidy.cmake
@@ -0,0 +1,25 @@
+find_program(clang_tidy "clang-tidy" REQUIRED)
+
+# Build clang-tidy command
+if(DEFINED ENV{CLANG_TIDY_SOURCES})
+    # Add source files from environment variable
+    message(STATUS "Taking sources from CLANG_TIDY_SOURCES environment variable")
+    list(APPEND clang_tidy_args $ENV{CLANG_TIDY_SOURCES})
+    list(APPEND clang_tidy_args "--")
+else()
+    # Add source files passed into script
+    list(APPEND clang_tidy_args ${SOURCES})
+    list(APPEND clang_tidy_args "--")
+    # Supplied sources will be .cpp files only; use header filter to include warnings from headers
+    list(APPEND clang_tidy_args "--header-filter='.*'")
+endif()
+
+# Add the rest of the command-line arguments
+list(APPEND clang_tidy_args ${CLANG_TIDY_ARGS})
+
+# Run the command and propogate error code
+list(JOIN clang_tidy_args "\" \"" clang_tidy_arg_str)
+message(STATUS "${clang_tidy} --version")
+execute_process(COMMAND "${clang_tidy}" --version)
+message(STATUS "\"${clang_tidy}\" \"${clang_tidy_arg_str}\"")
+execute_process(COMMAND "${clang_tidy}" ${clang_tidy_args} COMMAND_ERROR_IS_FATAL ANY)
