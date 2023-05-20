cb703e5918e0ab84107eec3300cb30a4ebe72694
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-05-20 21:27:09 +0200

    Add vcpkg toolchain for msan

---
+++ tools/cmake/toolchains/clang-msan.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_COMPILER clang++)
+set(VCPKG_C_FLAGS "-fsanitize=memory -ggdb -fno-omit-frame-pointer")
+set(VCPKG_CXX_FLAGS "-fsanitize=memory -ggdb -fno-omit-frame-pointer")
+set(VCPKG_LINKER_FLAGS "-fsanitize=memory")
--- tools/cmake/triplets/x64-linux-msan.cmake
+++ tools/cmake/triplets/x64-linux-msan.cmake
@@ -4,11 +4,4 @@ set(VCPKG_LIBRARY_LINKAGE static)
 
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
-message("VCPKG_CXX_FLAGS: ${VCPKG_CXX_FLAGS}")
-message("VCPKG_LINKER_FLAGS: ${VCPKG_LINKER_FLAGS}")
-set(VCPKG_CXX_FLAGS "-fsanitize=memory -ggdb -fno-omit-frame-pointer")
-set(VCPKG_C_FLAGS "-fsanitize=memory -ggdb -fno-omit-frame-pointer")
-set(VCPKG_LINKER_FLAGS "-fsanitize=memory")
-
-message("VCPKG_CXX_FLAGS: ${VCPKG_CXX_FLAGS}")
-message("VCPKG_LINKER_FLAGS: ${VCPKG_LINKER_FLAGS}")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../toolchains/clang-msan.cmake")
