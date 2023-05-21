00436a88268cacfcda44ff7113cf3e40d3484984
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-05-21 12:27:29 +0200

    Add cmake/modules path to toolchain

--- tools/cmake/toolchains/clang-msan.cmake
+++ tools/cmake/toolchains/clang-msan.cmake
@@ -5,6 +5,7 @@ string(APPEND CMAKE_C_FLAGS " -fsanitize=memory -ggdb -fno-omit-frame-pointer")
 string(APPEND CMAKE_CXX_FLAGS " -fsanitize=memory -ggdb -fno-omit-frame-pointer")
 string(APPEND CMAKE_LINKER_FLAGS " -fsanitize=memory")
 
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../modules)
 find_package(libcxx)
 
 string(APPEND CMAKE_CXX_FLAGS " -nostdinc++ -nostdlib++ -isystem \"${LIBCXX_INCLUDE_DIR}/c++/v1\"")
