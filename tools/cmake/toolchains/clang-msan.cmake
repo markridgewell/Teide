6e0b8bf96163ad54fc96430db4c1e020749cd468
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-05-21 12:08:38 +0200

    Fix incorrect package name

--- tools/cmake/toolchains/clang-msan.cmake
+++ tools/cmake/toolchains/clang-msan.cmake
@@ -5,7 +5,7 @@ string(APPEND CMAKE_C_FLAGS " -fsanitize=memory -ggdb -fno-omit-frame-pointer")
 string(APPEND CMAKE_CXX_FLAGS " -fsanitize=memory -ggdb -fno-omit-frame-pointer")
 string(APPEND CMAKE_LINKER_FLAGS " -fsanitize=memory")
 
-find_package(libcxx-msan)
+find_package(libcxx)
 
 string(APPEND CMAKE_CXX_FLAGS " -nostdinc++ -nostdlib++ -isystem \"${LIBCXX_INCLUDE_DIR}/c++/v1\"")
 string(
