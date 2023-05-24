708fb3c9980ad76a6c5f1892846c6290d2770e04
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-05-24 23:08:37 +0200

    Add PR comments for clang-tidy warnings

--- tools/cmake/ClangTidy.cmake
+++ tools/cmake/ClangTidy.cmake
@@ -3,18 +3,23 @@ include(tools/cmake/CompileCommands.cmake)
 function(td_add_clang_tidy)
     compile_commands()
 
-    list(APPEND clang_tidy_cmd "-xc++" "-std=c++${CMAKE_CXX_STANDARD}")
-
+    list(APPEND compiler_args "-xc++" "-std=c++${CMAKE_CXX_STANDARD}")
     foreach(def IN LISTS project_compile_definitions)
-        list(APPEND clang_tidy_cmd "-D${def}")
+        list(APPEND compiler_args "-D${def}")
     endforeach()
+
+    set(clang_tidy_args "--fix-notes" "--fix-errors" "--export-fixes=${CMAKE_BINARY_DIR}/clang-tidy-fixes.yaml")
 
     add_custom_target(
         ClangTidy
         COMMAND
-            ${CMAKE_COMMAND} "-DSOURCES='${project_sources}'" "-DINCLUDE_DIRS='${project_include_directories}'"
-            "-DSYSTEM_INCLUDE_DIRS='${project_system_include_directories}'" "-DCLANG_TIDY_ARGS='${clang_tidy_cmd}'" -P
-            "${SCRIPTS_DIR}/RunClangTidy.cmake"
+            ${CMAKE_COMMAND} #
+            "-DSOURCES='${project_sources}'" #
+            "-DINCLUDE_DIRS='${project_include_directories}'" #
+            "-DSYSTEM_INCLUDE_DIRS='${project_system_include_directories}'" #
+            "-DCLANG_TIDY_ARGS='${clang_tidy_args}'" #
+            "-DCOMPILER_ARGS='${compiler_args}'" #
+            -P "${SCRIPTS_DIR}/RunClangTidy.cmake"
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 
     set_target_properties(ClangTidy PROPERTIES FOLDER StaticAnalysis)
