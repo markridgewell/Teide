5eb6225cb81790db9117b4008e1ea2ac16928a7e
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-09-06 22:28:28 +0200

    Fix include dir for VMA-Hpp from vcpkg

--- tools/cmake/modules/FindVulkanMemoryAllocator-Hpp.cmake
+++ tools/cmake/modules/FindVulkanMemoryAllocator-Hpp.cmake
@@ -3,6 +3,12 @@ find_package(unofficial-vulkan-memory-allocator-hpp QUIET)
 if(unofficial-vulkan-memory-allocator-hpp_FOUND)
     add_library(VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp ALIAS
                 unofficial::VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp)
+
+    set_target_properties(
+        unofficial::VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp
+        PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+                   "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include/vulkan-memory-allocator-hpp")
+
     find_package_handle_standard_args(VulkanMemoryAllocator-Hpp DEFAULT_MSG)
 else()
     # Fall back to config mode to find the package locally
