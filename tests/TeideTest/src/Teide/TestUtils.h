bafc280a3832da7659714a8286372b51df7f65ef
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-09-14 01:50:33 +0200

    Improve test output (#36)
    * Add PrintTo helpers for byte and span
    
    * Reduce test log spam
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix formatting
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/TeideTest/src/Teide/TestUtils.h
+++ tests/TeideTest/src/Teide/TestUtils.h
@@ -34,6 +34,31 @@ void PrintTo(const Visitable auto& obj, std::ostream* os)
 }
 } // namespace Teide
 
+namespace std
+{
+inline void PrintTo(std::byte b, std::ostream* os)
+{
+    fmt::format_to(std::ostream_iterator<char>(*os), "0x{:02x}", static_cast<std::uint8_t>(b));
+}
+
+template <class T, std::size_t N>
+void PrintTo(const std::span<T, N>& span, std::ostream* os)
+{
+    *os << "{";
+    bool firstElem = true;
+    for (const auto& elem : span)
+    {
+        if (!firstElem)
+        {
+            *os << ",";
+        }
+        firstElem = false;
+        testing::internal::UniversalPrint(elem, os);
+    }
+    *os << "}";
+}
+} // namespace std
+
 #ifdef NDEBUG
 #    define EXPECT_UNREACHABLE(statement)                                                                              \
         if (false)                                                                                                     \
