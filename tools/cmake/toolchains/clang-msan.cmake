895d69dae265f32759452c27e1932917a9fa1127
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-10-08 01:24:34 +0200

    Set toolchain variables as env vars

--- tools/cmake/toolchains/clang-msan.cmake
+++ tools/cmake/toolchains/clang-msan.cmake
@@ -2,8 +2,36 @@ set(CMAKE_ASM_COMPILER clang-17)
 set(CMAKE_C_COMPILER clang-17)
 set(CMAKE_CXX_COMPILER clang++-17)
 
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../modules)
-find_package(libcxx REQUIRED)
+if(DEFINED ENV{LIBCXX_INCLUDE_DIR})
+    # Environment variable is set.
+    set(LIBCXX_INCLUDE_DIR $ENV{LIBCXX_INCLUDE_DIR})
+else()
+    # Environment variable is not set.
+    if (LIBCXX_INCLUDE_DIR)
+        # But CMake variable is set.
+        # Store it into the environment and use it.
+        set(ENV{LIBCXX_INCLUDE_DIR} ${LIBCXX_INCLUDE_DIR})
+    else()
+        # Neither environment nor CMake variable is set.
+        message(SEND_ERROR "LIBCXX_INCLUDE_DIR variable not set")
+    endif()
+endif()
+
+if(DEFINED ENV{LIBCXX_LIB_DIR})
+    # Environment variable is set.
+    set(LIBCXX_LIB_DIR $ENV{LIBCXX_LIB_DIR})
+else()
+    # Environment variable is not set.
+    if (LIBCXX_LIB_DIR)
+        # But CMake variable is set.
+        # Store it into the environment and use it.
+        set(ENV{LIBCXX_LIB_DIR} ${LIBCXX_LIB_DIR})
+    else()
+        # Neither environment nor CMake variable is set.
+        message(SEND_ERROR "LIBCXX_LIB_DIR variable not set")
+    endif()
+endif()
+
 message("LIBCXX_INCLUDE_DIR: ${LIBCXX_INCLUDE_DIR}")
 message("LIBCXX_LIB_DIR: ${LIBCXX_LIB_DIR}")
 
