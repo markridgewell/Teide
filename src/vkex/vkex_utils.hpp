8ca027d25fcac5e9037bfaeccf8ecd5e9ab7a076
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-10-11 02:20:02 +0200

    Add vkex::Join function for joining two views

--- src/vkex/vkex_utils.hpp
+++ src/vkex/vkex_utils.hpp
@@ -5,6 +5,8 @@
 #include <cstdint>
 #include <memory>
 #include <ranges>
+#include <tuple>
+#include <type_traits>
 
 #ifndef VKEX_ASSERT
 #    define VKEX_ASSERT VULKAN_HPP_ASSERT
@@ -74,5 +76,89 @@ private:
     std::uint32_t m_size = 0;
     std::unique_ptr<T[]> m_data = nullptr;
 };
+
+template <typename Range, typename T>
+concept SizedRangeOf = std::ranges::sized_range<Range> && std::same_as<std::ranges::range_value_t<Range>, T>;
+
+template <typename Range, typename T>
+concept SizedViewOf = SizedRangeOf<Range, T> && std::ranges::view<Range>;
+
+template <typename T, typename View1, typename View2>
+    requires(SizedViewOf<View1, T> && SizedViewOf<View2, T>)
+class JoinView : public std::ranges::view_interface<JoinView<T, View1, View2>>
+{
+public:
+    using value_type = T;
+
+    class Sentinel
+    {};
+
+    class Iterator
+    {
+    public:
+        using iterator_category = std::forward_iterator_tag;
+        using difference_type = std::ptrdiff_t;
+        using value_type = T;
+        using pointer = T*;
+        using reference = T&;
+
+        explicit Iterator(const std::pair<View1, View2>& views) :
+            m_iterators{views.first.begin(), views.second.begin()}, //
+            m_sentinels{views.first.end(), views.second.end()},
+            m_size{std::ranges::size(views.first) + std::ranges::size(views.second)}
+        {}
+
+        const T& operator*() const { return m_first ? *m_iterators.first : *m_iterators.second; }
+        const T* operator->() const { return std::addressof(operator*()); }
+
+        Iterator& operator++()
+        {
+            if (m_first)
+            {
+                ++m_iterators.first;
+                m_first = !(m_iterators.first == m_sentinels.first);
+            }
+            else
+            {
+                ++m_iterators.second;
+            }
+            return *this;
+        }
+
+        Iterator operator++(int)
+        {
+            Iterator tmp = *this;
+            operator++();
+            return tmp;
+        }
+
+        friend bool operator==(const Iterator& a, const Sentinel)
+        {
+            return !a.m_first && a.m_iterators.second == a.m_sentinels.second;
+        };
+
+    private:
+        std::pair<std::ranges::iterator_t<View1>, std::ranges::iterator_t<View2>> m_iterators;
+        std::pair<std::ranges::sentinel_t<View1>, std::ranges::sentinel_t<View2>> m_sentinels;
+        bool m_first = true;
+        std::size_t m_size = 0;
+    };
+
+    explicit JoinView(View1 view1, View2 view2) : m_views{view1, view2} {}
+
+    auto begin() const { return Iterator(m_views); }
+    auto end() const { return Sentinel(); }
+
+private:
+    std::pair<View1, View2> m_views;
+};
+
+template <typename... Ranges>
+    requires((std::ranges::sized_range<Ranges>) && ...)
+auto Join(const Ranges&... ranges)
+{
+    using T = std::common_type_t<std::ranges::range_value_t<Ranges>...>;
+    return JoinView<T, decltype(std::ranges::ref_view(ranges))...>(std::ranges::ref_view(ranges)...);
+}
 
 } // namespace vkex
--- tests/TeideTest/src/vkex/vkex_utils_test.cpp
+++ tests/TeideTest/src/vkex/vkex_utils_test.cpp
@@ -10,14 +10,14 @@
 using namespace vkex;
 using namespace testing;
 
-TEST(VulkanUtilsTest, ArrayDefaultConstruct)
+TEST(VkexUtilsTest, ArrayDefaultConstruct)
 {
     const Array<int> a;
     EXPECT_THAT(a.size(), Eq(0u));
     EXPECT_THAT(a.data(), IsNull());
 }
 
-TEST(VulkanUtilsTest, ArrayConstructFromRange)
+TEST(VkexUtilsTest, ArrayConstructFromRange)
 {
     const Array<int> a = std::views::iota(0, 5);
     EXPECT_THAT(a.size(), Eq(5u));
@@ -25,7 +25,7 @@ TEST(VulkanUtilsTest, ArrayConstructFromRange)
     EXPECT_THAT(a, ElementsAre(0, 1, 2, 3, 4));
 }
 
-TEST(VulkanUtilsTest, ArrayConstructSingleElement)
+TEST(VkexUtilsTest, ArrayConstructSingleElement)
 {
     const Array<int> a = 5;
     EXPECT_THAT(a.size(), Eq(1u));
@@ -33,41 +33,51 @@ TEST(VulkanUtilsTest, ArrayConstructSingleElement)
     EXPECT_THAT(a, ElementsAre(5));
 }
 
-TEST(VulkanUtilsTest, ArrayBeginEnd)
+TEST(VkexUtilsTest, ArrayBeginEnd)
 {
     const Array<int> a = std::views::iota(0, 5);
     EXPECT_THAT(a.begin(), Eq(a.data()));
     EXPECT_THAT(a.end(), Eq(std::next(a.data(), a.size())));
 }
 
-TEST(VulkanUtilsTest, ArrayConstBeginEnd)
+TEST(VkexUtilsTest, ArrayConstBeginEnd)
 {
     const Array<int> a = std::views::iota(0, 5);
     EXPECT_THAT(a.begin(), Eq(a.data()));
     EXPECT_THAT(a.end(), Eq(std::next(a.data(), a.size())));
 }
 
-TEST(VulkanUtilsTest, ArrayCbeginCend)
+TEST(VkexUtilsTest, ArrayCbeginCend)
 {
     const Array<int> a = std::views::iota(0, 5);
     EXPECT_THAT(a.begin(), Eq(a.data()));
     EXPECT_THAT(a.end(), Eq(std::next(a.data(), a.size())));
 }
 
-TEST(VulkanUtilsTest, ArrayMutableAlgorithm)
+TEST(VkexUtilsTest, ArrayMutableAlgorithm)
 {
     Array<int> a = std::views::iota(0, 5);
     std::ranges::fill(a, 42);
     EXPECT_THAT(a, ElementsAre(42, 42, 42, 42, 42));
 }
 
-TEST(VulkanUtilsTest, ArrayReset)
+TEST(VkexUtilsTest, ArrayReset)
 {
     Array<int> a;
     a.reset(10);
     EXPECT_THAT(a.size(), Eq(10u));
     EXPECT_THAT(a.data(), NotNull());
 }
+
+
+TEST(VkexUtilsTest, JoinTwoVectors)
+{
+    const auto a = std::vector<int>{1, 2, 3};
+    const auto b = std::vector<int>{4, 5};
+    const auto ab = Join(a, b);
+    EXPECT_THAT(ab, ElementsAre(1, 2, 3, 4, 5));
+}
+
 
 template <class T>
 T MakeHandle(std::uintptr_t value)
@@ -84,7 +94,7 @@ MATCHER_P(Handle, v, "handle with value")
     return NativeType(arg) == NativeType(static_cast<std::uintptr_t>(v)); // NOLINT(performance-no-int-to-ptr)
 }
 
-TEST(VulkanUtilsTest, ConvertSubmitInfo)
+TEST(VkexUtilsTest, ConvertSubmitInfo)
 {
     const SubmitInfo from = {
         .waitSemaphores = MakeHandle<vk::Semaphore>(1),
@@ -103,7 +113,7 @@ TEST(VulkanUtilsTest, ConvertSubmitInfo)
     EXPECT_THAT(to.pSignalSemaphores, Pointee(Handle(4)));
 }
 
-TEST(VulkanUtilsTest, ConvertPresentInfoKHR)
+TEST(VkexUtilsTest, ConvertPresentInfoKHR)
 {
     const PresentInfoKHR from = {
         .waitSemaphores = MakeHandle<vk::Semaphore>(1),
@@ -120,7 +130,7 @@ TEST(VulkanUtilsTest, ConvertPresentInfoKHR)
     EXPECT_THAT(to.pResults, IsNull());
 }
 
-TEST(VulkanUtilsTest, ConvertPresentInfoKHRWithResults)
+TEST(VkexUtilsTest, ConvertPresentInfoKHRWithResults)
 {
     Array<vk::Result> results;
     const PresentInfoKHR from = {
