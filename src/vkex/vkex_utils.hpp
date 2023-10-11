8f72a0674e3b3bcc7077a0c59c7204a20c302b66
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-10-12 00:57:27 +0200

    Make vkex.hpp a system include

--- src/vkex/vkex_utils.hpp
+++ src/vkex/vkex_utils.hpp
@@ -117,10 +117,7 @@ public:
             {
                 return m_iterators.first.operator*();
             }
-            else
-            {
-                return m_iterators.second.operator*();
-            }
+            return m_iterators.second.operator*();
         }
 
         Iterator& operator++()
