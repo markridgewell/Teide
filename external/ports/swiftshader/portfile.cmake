35632a3cf6f5440a2cba36d1fbe30a414a021217
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-10-14 21:29:51 +0200

    Make sure hooks directory exists

--- external/ports/swiftshader/portfile.cmake
+++ external/ports/swiftshader/portfile.cmake
@@ -6,6 +6,7 @@ vcpkg_from_github(
         44b46e6b209da195c937f9da08d8954bf64f6019755e0c5afc3e9bedbdd18c4eae4f26e5da1c2c4b3e0577287fb3abcb04aea22fb586d93ad199cc839305f9c3
     HEAD_REF master)
 
+file(MAKE_DIRECTORY ${SOURCE_PATH}/.git/hooks)
 file(TOUCH ${SOURCE_PATH}/.git/hooks/commit-msg)
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS -DSWIFTSHADER_BUILD_TESTS=OFF)
 vcpkg_cmake_install()
