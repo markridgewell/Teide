3d624a6a8c04e880160097c2054193acb9134100
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-10-14 20:26:54 +0200

    Use correct hash in portfile

--- external/ports/swiftshader/portfile.cmake
+++ external/ports/swiftshader/portfile.cmake
@@ -2,10 +2,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/swiftshader
     REF 400ac3a175a658d8157f8a363271ae7ab013c2ee
-    SHA512 0
+    SHA512
+        44b46e6b209da195c937f9da08d8954bf64f6019755e0c5afc3e9bedbdd18c4eae4f26e5da1c2c4b3e0577287fb3abcb04aea22fb586d93ad199cc839305f9c3
     HEAD_REF master)
 
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
