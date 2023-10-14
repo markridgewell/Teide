8a0e9947afce7e79f54dd156687b367d60895132
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-10-14 20:13:17 +0200

    Add custom port for SwiftShader to replace find module

---
+++ external/ports/swiftshader/portfile.cmake
@@ -0,0 +1,12 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO google/swiftshader
+    REF 400ac3a175a658d8157f8a363271ae7ab013c2ee
+    SHA512
+        0
+    HEAD_REF master)
+
+vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
+vcpkg_cmake_install()
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE.txt")
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
---
+++ tools/cmake/Option.cmake
@@ -0,0 +1,24 @@
+
+function(
+    option_feature
+    name
+    feature_name
+    description
+    default)
+    option(${name} "${description}" ${default})
+    if(${name})
+        list(APPEND VCPKG_MANIFEST_FEATURES "${feature_name}")
+    endif()
+endfunction()
+
+function(
+    option_enum
+    name
+    description
+    values)
+    list(GET values 0 first_value)
+    set(${name}
+        "${first_value}"
+        CACHE STRING "${description}")
+    set_property(CACHE ${name} PROPERTY STRINGS "${values}")
+endfunction()
