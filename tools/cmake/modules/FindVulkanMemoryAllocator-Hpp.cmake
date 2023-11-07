8f1aff2e8098fcbc3973dfdfdde8871ef2f536b4
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-11-07 07:02:16 +0100

    fix errors with prebuilt packages

--- tools/cmake/modules/FindVulkanMemoryAllocator-Hpp.cmake
+++ tools/cmake/modules/FindVulkanMemoryAllocator-Hpp.cmake
@@ -1,18 +1,12 @@
-# Find package in vcpkg ports under a different name
-find_package(unofficial-vulkan-memory-allocator-hpp QUIET)
-if(unofficial-vulkan-memory-allocator-hpp_FOUND)
-    add_library(VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp ALIAS
-                unofficial::VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp)
+# Try to find the package in CONFIG mode
+find_package(VulkanMemoryAllocator-Hpp CONFIG QUIET)
 
-    set_target_properties(
-        unofficial::VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp
-        PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                   "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include/vulkan-memory-allocator-hpp"
-                   INTERFACE_SYSTEM_INCLUDE_DIRECTORIES
-                   "${_VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include/vulkan-memory-allocator-hpp")
-
+if(NOT VulkanMemoryAllocator-Hpp_FOUND)
+    # Find package in vcpkg ports under a different name
+    find_package(unofficial-vulkan-memory-allocator-hpp)
+    if(NOT TARGET VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp)
+        add_library(VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp ALIAS
+                    unofficial::VulkanMemoryAllocator-Hpp::VulkanMemoryAllocator-Hpp)        
+    endif()
     find_package_handle_standard_args(VulkanMemoryAllocator-Hpp DEFAULT_MSG)
-else()
-    # Fall back to config mode to find the package locally
-    find_package(VulkanMemoryAllocator-Hpp CONFIG)
 endif()
