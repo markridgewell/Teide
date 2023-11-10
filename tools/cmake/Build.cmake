6c926e90f02978de55bb25c6d2928b8788f19f1a
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-11-10 02:24:17 +0100

    change command for copying dlls

--- tools/cmake/Build.cmake
+++ tools/cmake/Build.cmake
@@ -35,7 +35,7 @@ function(_copy_install_files target)
         add_custom_command(
             TARGET ${target}
             POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_RUNTIME_DLLS:${target}> $<TARGET_FILE_DIR:${target}>
+            COMMAND ${CMAKE_COMMAND} -E copy -t $<TARGET_FILE_DIR:${target}> $<TARGET_RUNTIME_DLLS:${target}>
             COMMAND_EXPAND_LISTS)
     endif()
 endfunction()
