e50cc3dc9ac240265c3d176aea7ef037b1a4f7da
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-11-16 18:58:23 +0100

    fix patch

--- external/ports/swiftshader/portfile.cmake
+++ external/ports/swiftshader/portfile.cmake
@@ -1,3 +1,6 @@
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO google/swiftshader
@@ -6,8 +9,6 @@ vcpkg_from_github(
         44b46e6b209da195c937f9da08d8954bf64f6019755e0c5afc3e9bedbdd18c4eae4f26e5da1c2c4b3e0577287fb3abcb04aea22fb586d93ad199cc839305f9c3
     HEAD_REF master
     PATCHES buildfixes.patch)
-
-set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
 
 file(MAKE_DIRECTORY ${SOURCE_PATH}/.git/hooks)
 file(TOUCH ${SOURCE_PATH}/.git/hooks/commit-msg)
