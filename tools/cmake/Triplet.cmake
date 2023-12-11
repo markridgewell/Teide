b0198fc253db3a72601f255206ba2fadb4c3a360
markridgewell <65724979+markridgewell@users.noreply.github.com>
2023-12-12 00:52:44 +0100

    Add triplet for Termux (#117)

--- tools/cmake/Triplet.cmake
+++ tools/cmake/Triplet.cmake
@@ -1,13 +1,40 @@
+if(DEFINED VCPKG_TARGET_TRIPLET)
+    return()
+endif()
+
 if(NOT DEFINED TEIDE_SANITIZER)
     message(SEND_ERROR "Triplet determination requires cache variable TEIDE_SANITIZER, which was not defined.")
 endif()
 
 if(LINUX)
-    if(TEIDE_SANITIZER STREQUAL "UBSAN")
-        set(VCPKG_TARGET_TRIPLET "x64-linux-ubsan")
+    # Detect host architecture
+    find_program(UNAME_PATH uname REQUIRED)
+    execute_process(COMMAND "${UNAME_PATH}" -m OUTPUT_VARIABLE UNAME_OUTPUT)
+    string(STRIP "${UNAME_OUTPUT}" UNAME_OUTPUT)
+    if(UNAME_OUTPUT STREQUAL x86_64)
+        set(ARCH x64)
+    elseif(UNAME_OUTPUT STREQUAL aarch64)
+        set(ARCH arm64)
     else()
-        set(VCPKG_TARGET_TRIPLET "x64-linux")
+        message(FATAL_ERROR "Unrecognised architecture \"${UNAME_OUTPUT}\"")
+    endif()
+
+    # Detect termux
+    set(ENVIRONMENT linux)
+    find_program(TERMUX termux-setup-storage NO_CACHE)
+    if(TERMUX)
+        message(Termux!)
+        set(ENVIRONMENT termux)
+    endif()
+
+    # Set triplet taking sanitizer into account
+    if(TEIDE_SANITIZER STREQUAL "UBSAN")
+        set(VCPKG_TARGET_TRIPLET "${ARCH}-${ENVIRONMENT}-ubsan")
+    else()
+        set(VCPKG_TARGET_TRIPLET "${ARCH}-${ENVIRONMENT}")
     endif()
 elseif(WIN32)
     set(VCPKG_TARGET_TRIPLET "x64-windows-static-md")
 endif()
+
+set(VCPKG_HOST_TRIPLET ${VCPKG_TARGET_TRIPLET})
---
+++ tools/cmake/triplets/arm64-termux.cmake
@@ -0,0 +1,18 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_HOST_SYSTEM_NAME=Linux;-DCMAKE_CROSSCOMPILING=OFF")
+
+set(undef_android "-U__ANDROID__ -UANDROID")
+
+if(PORT STREQUAL "sdl2")
+    set(VCPKG_C_FLAGS "${undef_android} -DSDL_main=main")
+    set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS}")
+    list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DSDL_KMSDRM=OFF")
+endif()
+
+if(PORT STREQUAL "swiftshader")
+    set(VCPKG_CXX_FLAGS "${undef_android} -lX11 -landroid-shmem -Wl,--undefined-version")
+endif()
