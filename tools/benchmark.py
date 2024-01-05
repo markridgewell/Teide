47f579f3df2c9a6ac2339fc8c16eb581cab51eae
markridgewell <65724979+markridgewell@users.noreply.github.com>
2024-01-05 05:19:10 +0100

    add missing parameters

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -66,20 +66,20 @@ def benchmark_commit(ref_name, preset, out_dir: Path, definitions: list[str]):
 
 def benchmark_compare(ref1_name: str, ref2_name: str, preset, out_dir: Path, compare: str, definitions: list[str]):
     output_file = out_dir / (ref1_name + '_' + ref2_name + '.json')
-    result1 = benchmark_commit(ref1_name, preset, out_dir)
+    result1 = benchmark_commit(ref1_name, preset, out_dir, definitions)
     print()
     result2 = benchmark_commit(ref2_name, preset, out_dir, definitions)
     print()
     print(run_process([sys.executable, compare, '--display_aggregates_only', '--dump_to_json', output_file, 'benchmarks', result1, result2]))
 
 
-def benchmark_prs(preset, out_dir: Path, compare: str):
+def benchmark_prs(preset, out_dir: Path, compare: str, definitions: list[str]):
     prs = json.loads(run_process(['gh', 'pr', 'list', '--json', 'baseRefName,headRefName,headRefOid,title,number']))
     for pr in prs:
         print("#{number} {title}\n  Head: {headRefOid} ({headRefName})".format(**pr))
         merge_base = run_process(['git', 'merge-base', pr['headRefOid'], 'origin/'+pr['baseRefName']]).strip()
         print(f"  Base: {merge_base} ({pr['baseRefName']})")
-        benchmark_compare(merge_base, pr['headRefOid'], preset, out_dir, compare)
+        benchmark_compare(merge_base, pr['headRefOid'], preset, out_dir, compare, definitions)
 
 
 def add_build_options(cmd):
@@ -122,7 +122,6 @@ if __name__ == '__main__':
     opts = parser.parse_args()
     func = opts.func
     delattr(opts, 'func')
-    print(vars(opts))
     try:
         func(**vars(opts))
     except subprocess.CalledProcessError as e:
