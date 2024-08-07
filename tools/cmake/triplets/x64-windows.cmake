8c32a70ef7d23e2f2e8c605925bea182ab58c32b
markridgewell <65724979+markridgewell@users.noreply.github.com>
2024-08-07 22:32:36 +0200

    Disable compiler tracking for all vcpkg triplets (#152)

--- tools/cmake/triplets/x64-windows.cmake
+++ tools/cmake/triplets/x64-windows.cmake
@@ -2,6 +2,8 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
+set(VCPKG_DISABLE_COMPILER_TRACKING ON)
+
 if(PORT EQUAL "swiftshader")
     set(VCPKG_LIBRARY_LINKAGE static)
 endif()
