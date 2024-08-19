bc6f50cff4271f8489cf58c088ec7500ba91248a
markridgewell <65724979+markridgewell@users.noreply.github.com>
2024-08-19 22:31:41 +0200

    update toolchain

--- tools/vcpkg/toolchains/clang-msan.cmake
+++ tools/vcpkg/toolchains/clang-msan.cmake
@@ -25,6 +25,8 @@ set(EXTRA_CXX_FLAGS
     " -nostdinc++ -nostdlib++ -isystem \"${LIBCXX_INCLUDE_DIR}/c++/v1\" -L \"${LIBCXX_LIB_DIR}\" \"-Wl,-rpath,${LIBCXX_LIB_DIR}\" -lc++ -lc++abi -lunwind -Wno-unused-command-line-argument"
 )
 
-string(APPEND CMAKE_C_FLAGS " -fsanitize=memory -ggdb -fno-omit-frame-pointer")
-string(APPEND CMAKE_CXX_FLAGS " -fsanitize=memory -ggdb -fno-omit-frame-pointer ${EXTRA_CXX_FLAGS}")
-string(APPEND CMAKE_LINKER_FLAGS " -fsanitize=memory -ggdb -fno-omit-frame-pointer ${EXTRA_CXX_FLAGS}")
+string(APPEND CMAKE_C_FLAGS " -fsanitize=memory -fsanitize-memory-track-origins -ggdb -fno-omit-frame-pointer")
+string(APPEND CMAKE_CXX_FLAGS
+       " -fsanitize=memory -fsanitize-memory-track-origins -ggdb -fno-omit-frame-pointer ${EXTRA_CXX_FLAGS}")
+string(APPEND CMAKE_LINKER_FLAGS
+       " -fsanitize=memory -fsanitize-memory-track-origins -ggdb -fno-omit-frame-pointer ${EXTRA_CXX_FLAGS}")
