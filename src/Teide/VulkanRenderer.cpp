41fdaaf9a1f2eac70bfa046c478b6b67ba5ee15a
markridgewell <65724979+markridgewell@users.noreply.github.com>
2024-10-28 02:52:14 +0100

    fix initialisation of parameter blocks

--- src/Teide/VulkanRenderer.cpp
+++ src/Teide/VulkanRenderer.cpp
@@ -458,7 +458,6 @@ void VulkanRenderer::RecordRenderObjectCommands(
     commandBufferWrapper.AddReference(obj.materialParameters);
     commandBufferWrapper.AddReference(obj.pipeline);
 
-    m_device.InitParameterBlock(obj.materialParameters);
     if (const auto dset = GetDescriptorSet(obj.materialParameters))
     {
         commandBuffer.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, pipeline.layout, 2, dset, {});
@@ -507,7 +506,8 @@ std::optional<SurfaceImage> VulkanRenderer::AddSurfaceToPresent(VulkanSurface& s
 
 vk::DescriptorSet VulkanRenderer::GetDescriptorSet(const ParameterBlock& parameterBlock) const
 {
-    const auto& parameterBlockImpl = m_device.GetImpl(parameterBlock);
+    auto& parameterBlockImpl = m_device.GetImpl(parameterBlock);
+    m_device.InitParameterBlock(parameterBlockImpl);
     return parameterBlockImpl.descriptorSet.get();
 }
 
