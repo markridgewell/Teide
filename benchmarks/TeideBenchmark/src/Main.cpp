8693a7b50860f88d20cbb5014c7d017315176036
markridgewell <65724979+markridgewell@users.noreply.github.com>
2024-11-27 00:12:05 +0100

    test cppcheck

--- benchmarks/TeideBenchmark/src/Main.cpp
+++ benchmarks/TeideBenchmark/src/Main.cpp
@@ -16,3 +16,12 @@ int main(int argc, char** argv)
     ::benchmark::Initialize(&argc, argv);
     ::benchmark::RunSpecifiedBenchmarks();
 }
+
+void foo(int x)
+{
+    int buf[10];
+    buf[x] = 0; // <- ERROR
+    if (x == 1000)
+    {
+    }
+}
