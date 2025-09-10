482fef85f848ba2ba886008fc581d2c9baafc3fc
markridgewell <65724979+markridgewell@users.noreply.github.com>
2025-09-10 22:39:10 +0200

    fixup

--- tools/cmake/Ccache.cmake
+++ tools/cmake/Ccache.cmake
@@ -1,7 +1,7 @@
 function(_enable_ccache_msvc)
     find_program(ccache_exe ccache)
     if(ccache_exe)
-        file(COPY_FILE ${ccache_exe} ${CMAKE_BINARY_DIR}/cl.exe ONLY_IF_DIFFERENT)
+        # file(COPY_FILE ${ccache_exe} ${CMAKE_BINARY_DIR}/cl.exe ONLY_IF_DIFFERENT)
 
         # By default Visual Studio generators will use /Zi which is not compatible with ccache, so tell Visual Studio to
         # use /Z7 instead.
