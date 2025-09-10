723e14d4a44361ee17bdfca2e9f4c0a28b68c15e
markridgewell <65724979+markridgewell@users.noreply.github.com>
2025-09-10 19:27:34 +0200

    Add support for ccache with msvc

---
+++ tools/cmake/Ccache.cmake
@@ -0,0 +1,14 @@
+if(MSVC)
+    find_program(ccache_exe ccache)
+    if(ccache_exe)
+        file(COPY_FILE ${ccache_exe} ${CMAKE_BINARY_DIR}/cl.exe ONLY_IF_DIFFERENT)
+
+        # By default Visual Studio generators will use /Zi which is not compatible with ccache, so tell Visual Studio to
+        # use /Z7 instead.
+        message(STATUS "Setting MSVC debug information format to 'Embedded'")
+        set(CMAKE_MSVC_DEBUG_INFORMATION_FORMAT "$<$<CONFIG:Debug,RelWithDebInfo>:Embedded>")
+
+        set(CMAKE_VS_GLOBALS "CLToolExe=cl.exe" "CLToolPath=${CMAKE_BINARY_DIR}" "UseMultiToolTask=true"
+                             "DebugInformationFormat=OldStyle")
+    endif()
+endif()
