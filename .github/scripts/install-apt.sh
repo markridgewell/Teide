69d4df062240fc992854c9143bac5d0113abbdb7
markridgewell <65724979+markridgewell@users.noreply.github.com>
2025-09-15 10:46:22 +0200

    Make script for installing apt packages

---
+++ .github/scripts/install-apt.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+  echo "No packages selected to install"
+  exit
+fi
+
+packages=$@
+tempdir=${RUNNER_TEMP:-${TMPDIR}}/install-apt
+
+mkdir -p ${tempdir}
+
+function install_package() {
+  start=`date +%s`
+  tempfile=${tempdir}/$1.out.log
+  > ${tempfile}
+  sudo apt-get install -y $1 >${tempfile} 2>&1
+  end=`date +%s`
+
+  echo "::group::$1 $(( end - $start ))s"
+  cat ${tempfile}
+  echo "::endgroup::"
+}
+
+# If clang packages requested, add the appropriate llvm repository
+LLVM_VERSIONS=$(echo ${packages} | tr ' ' '\n' | sed -n 's/^clang.*-\([1-9][0-9]*\)$/\1/p' | sort | uniq)
+if [ -n "${LLVM_VERSIONS}" ]; then
+  echo "Clang requested, adding LLVM repositories..."
+  wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+  UBUNTU_CODENAME=`sudo cat /etc/os-release | sed -n s/UBUNTU_CODENAME=//p`
+  for VERSION in ${LLVM_VERSIONS}; do
+    REPO="deb http://apt.llvm.org/${UBUNTU_CODENAME}/ llvm-toolchain-${UBUNTU_CODENAME}-${VERSION} main"
+    echo Adding repository "${REPO}"
+    sudo add-apt-repository "${REPO}"
+  done
+  sudo apt-get update
+  sudo apt-get install -y llvm
+fi
+
+echo "Installing apt packages: ${packages}"
+for i in ${packages}; do
+  install_package $i
+done
