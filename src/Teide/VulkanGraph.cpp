c763eab2c93b1e36b09710eef75a6a84e507ac37
markridgewell <65724979+markridgewell@users.noreply.github.com>
2025-09-25 20:32:38 +0200

    add sampled usage to transient textures in graph

--- src/Teide/VulkanGraph.cpp
+++ src/Teide/VulkanGraph.cpp
@@ -156,7 +156,7 @@ void ExecuteGraph(VulkanGraph& graph, VulkanDevice& device, VulkanRenderer& rend
     {
         VulkanTexture& texture = device.GetImpl(node.texture);
         using enum vk::ImageUsageFlagBits;
-        device.CreateTextureImpl(texture, eTransferSrc | eTransferDst);
+        device.CreateTextureImpl(texture, eTransferSrc | eTransferDst | eSampled);
     }
 
     // 2. Create staging buffers
@@ -221,7 +221,7 @@ void ExecuteGraph(VulkanGraph& graph, VulkanDevice& device, VulkanRenderer& rend
                 const TextureData& data = sourceNode->data;
                 VulkanTexture& texture = device.GetImpl(node.texture);
                 using enum vk::ImageUsageFlagBits;
-                auto state = device.CreateTextureImpl(texture, eTransferSrc | eTransferDst);
+                auto state = device.CreateTextureImpl(texture, eTransferSrc | eTransferDst | eSampled);
 
                 // Copy staging buffer to image
                 TransitionImageLayout(
