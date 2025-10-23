8a28ca828ef4aecf7b2b63a289b16651c8a028d1
markridgewell <65724979+markridgewell@users.noreply.github.com>
2025-10-23 08:52:37 +0200

    test stdexec

--- tests/TeideTest/src/Main.cpp
+++ tests/TeideTest/src/Main.cpp
@@ -303,8 +303,33 @@ int main(int argc, char** argv)
     }
 }
 #else
+#    include <exec/static_thread_pool.hpp>
+#    include <stdexec/execution.hpp>
+
 int main(int argc, char** argv)
 {
+    // Declare a pool of 3 worker threads:
+    exec::static_thread_pool pool(3);
+
+    // Get a handle to the thread pool:
+    auto sched = pool.get_scheduler();
+
+    // Describe some work:
+    // Creates 3 sender pipelines that are executed concurrently by passing to `when_all`
+    // Each sender is scheduled on `sched` using `on` and starts with `just(n)` that creates a
+    // Sender that just forwards `n` to the next sender.
+    // After `just(n)`, we chain `then(fun)` which invokes `fun` using the value provided from `just()`
+    // Note: No work actually happens here. Everything is lazy and `work` is just an object that statically
+    // represents the work to later be executed
+    auto fun = [](int i) { return i * i; };
+    auto work = stdexec::start_on(sched, stdexec::just(0) | stdexec::then(fun));
+
+    // Launch the work and wait for the result
+    auto [i] = stdexec::sync_wait(work).value();
+
+    // Print the results:
+    fmt::println("{}", i);
+
     return Run(argc, argv);
 }
 #endif
