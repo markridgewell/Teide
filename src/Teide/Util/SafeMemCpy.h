87e132d3af4057c9f17f9d3fe384ec49295eb420
markridgewell <65724979+markridgewell@users.noreply.github.com>
2025-11-28 23:37:18 +0100

    fix errors in safememcpy

--- src/Teide/Util/SafeMemCpy.h
+++ src/Teide/Util/SafeMemCpy.h
@@ -10,9 +10,8 @@ template <class Dest, class Source>
     requires(
         std::ranges::contiguous_range<Dest>
         && std::ranges::sized_range<Dest>
-        &&
-        // !std::ranges::constant_range<Dest> && // C++23
-        std::ranges::contiguous_range<Source>
+        // && !std::ranges::constant_range<Dest> // C++23
+        && std::ranges::contiguous_range<Source>
         && std::ranges::sized_range<Source>
         && !(
             sizeof(std::ranges::range_value_t<Dest>)
@@ -21,11 +20,9 @@ template <class Dest, class Source>
             && sizeof(std::ranges::range_value_t<Source>)
             % sizeof(std::ranges::range_value_t<Dest>)
             != 0))
-void SafeMemCpy(Dest&& dest, const Source& source)
+void SafeMemCpy(Dest& dest, const Source& source)
 {
     namespace sr = std::ranges;
-    std::cout << sr::size(dest) << " * " << sizeof(sr::range_value_t<Dest>) << "\n";
-    std::cout << sr::size(source) << " * " << sizeof(sr::range_value_t<Source>) << "\n";
     const auto destSize = static_cast<std::size_t>(sr::size(dest)) * sizeof(sr::range_value_t<Dest>);
     const auto sourceSize = static_cast<std::size_t>(sr::size(source)) * sizeof(sr::range_value_t<Source>);
     const auto size = std::min(destSize, sourceSize);
