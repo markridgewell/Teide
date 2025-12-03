ad52ad47fb3b0fd5a185461612d26e0aa7fe471c
markridgewell <65724979+markridgewell@users.noreply.github.com>
2025-12-03 06:01:30 +0100

    tidy test runner script

--- tools/cmake/scripts/RunTest.cmake
+++ tools/cmake/scripts/RunTest.cmake
@@ -7,8 +7,13 @@ if("$ENV{TEST_COVERAGE}")
             cmake_path(NATIVE_PATH path native_path)
             list(APPEND sources "--source=${native_path}")
         endforeach()
-        set(RUNNER_COMMAND "$ENV{OPENCPPCOVERAGE}" "--export_type=binary:${coverage_dir}/${test_name}.cov"
-                           "--modules=*.exe" ${sources} --cover_children --continue_after_cpp_exception --)
+        set(RUNNER_COMMAND
+            "$ENV{OPENCPPCOVERAGE}" #
+            "--export_type=binary:${coverage_dir}/${test_name}.cov" #
+            "--modules=*.exe" ${sources} #
+            --cover_children #
+            --continue_after_cpp_exception #
+            --)
     elseif(COMPILER STREQUAL "Clang")
         file(APPEND "$ENV{COVERAGE_DIR}/test_binaries.txt" "${TEST_BINARY}\n")
         list(JOIN SOURCES "\n" source_list)
