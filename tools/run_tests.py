57edc954d31fa85cd88212656a8136fcf1989d16
markridgewell <65724979+markridgewell@users.noreply.github.com>
2026-02-15 02:49:16 +0100

    restore test script with proper ordering of tests

--- tools/run_tests.py
+++ tools/run_tests.py
@@ -4,7 +4,6 @@ import sys
 import subprocess
 import json
 import os
-import pprint
 
 if len(sys.argv) != 2:
     print(f'usage: {sys.argv[0]} <test-dir>')
@@ -15,19 +14,50 @@ test_dir = sys.argv[1]
 cmd = ['ctest', '--test-dir', test_dir, '--build-config', 'Debug', '--no-tests=error', '--show-only=json-v1']
 output = subprocess.check_output(cmd, encoding='utf-8')
 data = json.loads(output)
-pprint.pprint(data)
-tests = data['tests']
 
 num_failures = 0
 
 test_env = os.environ.copy()
 test_env["GTEST_COLOR"] = "1"
 
+tests = data['tests']
+sequence = []
+
+def find_test(name):
+    global tests
+    for test in tests:
+        if test['name'] == name:
+            return test
+    raise KeyError
+
+def get_dependencies(test):
+    if 'properties' not in test:
+        return []
+    for prop in test['properties']:
+        if prop['name'] == 'DEPENDS':
+            return prop['value']
+    return []
+
+def add_to_sequence(test_name):
+    global sequence
+    if test_name in sequence:
+        return
+    test = find_test(test_name)
+    for dep in get_dependencies(test):
+        add_to_sequence(dep)
+    sequence += [test_name]
+
 for test in tests:
-    test_name = test['name']
+    add_to_sequence(test['name'])
+
+print('Test sequence:', sequence)
+print()
+
+for test_name in sequence:
+    test = find_test(test_name)
     test_cmd = test['command']
     print(f'Running test {test_name}')
-    print(subprocess.list2cmdline(test_cmd), flush=True)
+    # print(subprocess.list2cmdline(test_cmd), flush=True)
     retcode = subprocess.run(test_cmd, env=test_env).returncode
     if retcode != 0:
         num_failures = num_failures + 1
