036b065ea45df451ac75bd479895e96bd223468e
markridgewell <65724979+markridgewell@users.noreply.github.com>
2026-07-23 04:21:57 +0200

    Move RenderDoc hooks into Teide library (#267)

--- tests/RenderTest/FileLists.cmake
+++ tests/RenderTest/FileLists.cmake
@@ -1,7 +1,1 @@
-set(sources
-    src/Main.cpp
-    src/RenderDocHooks.cpp
-    src/RenderDocHooks.h
-    src/RenderTest.cpp
-    src/RenderTest.h
-    src/Tests.cpp)
+set(sources src/Main.cpp src/RenderTest.cpp src/RenderTest.h src/Tests.cpp)
